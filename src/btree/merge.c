bc3e8a136c3ba9fd48f893bcdefa667f89717130
Alexander Korotkov <akorotkov@postgresql.org>
2022-07-07 12:39:59 +0300

    Forbid using page under checkpoint as the right page for merge
    If we use hikeyBlkno as right page for merge, we may loose tracking of the
    page containing the hikey of the autonomous page.

--- src/btree/merge.c
+++ src/btree/merge.c
@@ -375,6 +375,7 @@ btree_try_merge_and_unlock(BTreeDescr *desc, OInMemoryBlkno blkno,
 
 				if (!O_PAGE_IS(right, PRE_CLEANUP) &&
 					!RightLinkIsValid(BTREE_PAGE_GET_RIGHTLINK(right)) &&
+					!page_is_under_checkpoint(desc, right_blkno) &&
 					io_num < 0)
 				{
 					merged = btree_try_merge_pages(desc, parent_blkno, &key,
