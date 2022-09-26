b58c5568026b95a940e9a27a9b8946e8f9ae04ef
Pavel Borisov <pashkin.elfe@gmail.com>
2022-09-26 14:12:53 +0400

    Fix rel naming in parallel scan text

--- sql/parallel_scan.sql
+++ sql/parallel_scan.sql
@@ -8,7 +8,7 @@ SET enable_seqscan = ON;
 SET enable_bitmapscan = OFF;
 SET enable_indexscan = OFF;
 
-CREATE TABLE bitmap_test
+CREATE TABLE seq_scan_test
 (
 	id serial primary key,
 	i int4
@@ -19,69 +19,69 @@ $$
 	SELECT substr(sha256(($1::text || ' ' || $2::text)::bytea)::text,2,16)::bit(52)::bigint::float8 / pow(2.0, 52.0);
 $$ LANGUAGE sql;
 
-ALTER SEQUENCE bitmap_test_id_seq RESTART WITH 100000;
+ALTER SEQUENCE seq_scan_test_id_seq RESTART WITH 100000;
 
-INSERT INTO bitmap_test (i)
+INSERT INTO seq_scan_test (i)
 	SELECT pseudo_random(1, v) * 1200000 FROM generate_series(1,300000) v;
-ANALYZE bitmap_test;
+ANALYZE seq_scan_test;
 
-CREATE INDEX bitmap_test_ix1 ON bitmap_test (i);
-
-SET max_parallel_workers_per_gather = 0;
-EXPLAIN (COSTS OFF) SELECT count(*) FROM bitmap_test WHERE i < 100;
-SELECT count(*) FROM bitmap_test WHERE i < 100;
-
-SET max_parallel_workers_per_gather = 5;
-EXPLAIN (COSTS OFF) SELECT count(*) FROM bitmap_test WHERE i < 100;
-SELECT count(*) FROM bitmap_test WHERE i < 100;
+CREATE INDEX seq_scan_test_ix1 ON seq_scan_test (i);
 
 SET max_parallel_workers_per_gather = 0;
-EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 100 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 100 ORDER BY i,id LIMIT 20;
+EXPLAIN (COSTS OFF) SELECT count(*) FROM seq_scan_test WHERE i < 100;
+SELECT count(*) FROM seq_scan_test WHERE i < 100;
 
 SET max_parallel_workers_per_gather = 5;
-EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 100 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 100 ORDER BY i,id LIMIT 20;
-
-SET max_parallel_workers_per_gather = 0;
-EXPLAIN (COSTS OFF) SELECT count(*) FROM bitmap_test WHERE i < 1000;
-SELECT count(*) FROM bitmap_test WHERE i < 1000;
-
-SET max_parallel_workers_per_gather = 5;
-EXPLAIN (COSTS OFF) SELECT count(*) FROM bitmap_test WHERE i < 1000;
-SELECT count(*) FROM bitmap_test WHERE i < 1000;
+EXPLAIN (COSTS OFF) SELECT count(*) FROM seq_scan_test WHERE i < 100;
+SELECT count(*) FROM seq_scan_test WHERE i < 100;
 
 SET max_parallel_workers_per_gather = 0;
 EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
+	SELECT * FROM seq_scan_test WHERE i < 100 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 100 ORDER BY i,id LIMIT 20;
 
 SET max_parallel_workers_per_gather = 5;
 EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
+	SELECT * FROM seq_scan_test WHERE i < 100 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 100 ORDER BY i,id LIMIT 20;
+
+SET max_parallel_workers_per_gather = 0;
+EXPLAIN (COSTS OFF) SELECT count(*) FROM seq_scan_test WHERE i < 1000;
+SELECT count(*) FROM seq_scan_test WHERE i < 1000;
+
+SET max_parallel_workers_per_gather = 5;
+EXPLAIN (COSTS OFF) SELECT count(*) FROM seq_scan_test WHERE i < 1000;
+SELECT count(*) FROM seq_scan_test WHERE i < 1000;
 
 SET max_parallel_workers_per_gather = 0;
 EXPLAIN (COSTS OFF)
-	SELECT count(*) FROM bitmap_test WHERE i < 1000 OR i > 13000;
-SELECT count(*) FROM bitmap_test WHERE i < 1000 OR i > 13000;
+	SELECT * FROM seq_scan_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
 
 SET max_parallel_workers_per_gather = 5;
 EXPLAIN (COSTS OFF)
-	SELECT count(*) FROM bitmap_test WHERE i < 1000 OR i > 13000;
-SELECT count(*) FROM bitmap_test WHERE i < 1000 OR i > 13000;
+	SELECT * FROM seq_scan_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 1000 ORDER BY i,id LIMIT 20;
 
 SET max_parallel_workers_per_gather = 0;
 EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
+	SELECT count(*) FROM seq_scan_test WHERE i < 1000 OR i > 13000;
+SELECT count(*) FROM seq_scan_test WHERE i < 1000 OR i > 13000;
 
 SET max_parallel_workers_per_gather = 5;
 EXPLAIN (COSTS OFF)
-	SELECT * FROM bitmap_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
-SELECT * FROM bitmap_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
+	SELECT count(*) FROM seq_scan_test WHERE i < 1000 OR i > 13000;
+SELECT count(*) FROM seq_scan_test WHERE i < 1000 OR i > 13000;
+
+SET max_parallel_workers_per_gather = 0;
+EXPLAIN (COSTS OFF)
+	SELECT * FROM seq_scan_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
+
+SET max_parallel_workers_per_gather = 5;
+EXPLAIN (COSTS OFF)
+	SELECT * FROM seq_scan_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
+SELECT * FROM seq_scan_test WHERE i < 1000 OR i > 13000 ORDER BY i,id LIMIT 20;
 
 SET max_parallel_workers_per_gather = 0;
 RESET min_parallel_table_scan_size;
