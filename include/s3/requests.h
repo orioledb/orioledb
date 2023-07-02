20f8ccd26dfb35824ca301a5cd5260db1e713944
Kiryl <kiryl@orioledata.com>
2023-07-03 00:17:54 +0300

    Implement S3 requests
    This commit provides function for S3 GET and PUT requests.  The requests
    are made with CURL library.  The modern AWS4-HMAC-SHA256 signature is
    implemented using OpenSSL.  SQL-level s3_get() and s3_put() functions are
    exposed for debugging purposes.

---
+++ include/s3/requests.h
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ *
+ * requests.h
+ *		Declarations for S3 requests.
+ *
+ * Copyright (c) 2023, OrioleDATA Inc.
+ *
+ * IDENTIFICATION
+ *	  contrib/orioledb/include/s3/requests.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef __S3_REQUESTS_H__
+#define __S3_REQUESTS_H__
+
+extern void s3_put_file(char *objectname, char *filename);
+extern void s3_put_file_part(char *objectname, char *filename, int partnum);
+
+#endif							/* __S3_REQUESTS_H__ */
