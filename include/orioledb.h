f104e09f4d1dfe3deaf442a7e883717d16340a48
Alexander Korotkov <akorotkov@postgresql.org>
2023-10-29 01:49:57 +0300

    Stamp beta 3

--- include/orioledb.h
+++ include/orioledb.h
@@ -32,7 +32,7 @@
 #include "utils/rel.h"
 #include "utils/relcache.h"
 
-#define ORIOLEDB_VERSION "OrioleDB public beta 2"
+#define ORIOLEDB_VERSION "OrioleDB public beta 3"
 #define ORIOLEDB_BINARY_VERSION 4
 #define ORIOLEDB_DATA_DIR "orioledb_data"
 #define ORIOLEDB_UNDO_DIR "orioledb_undo"
