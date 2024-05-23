dbc852309d05446afc98899f6f6bb577ad465f1d
Ilya Kobets <i.kobets@orioledb.com>
2024-05-23 15:58:02 +0200

    Implemented amdelete [skip ci]

--- src/indexam/handler.c
+++ src/indexam/handler.c
@@ -880,6 +880,115 @@ orioledb_amdelete(Relation rel, Datum *values, bool *isnull,
 
 	return result.success;
 }
+bool
+orioledb_amdelete(Relation rel, Datum *values, bool *isnull,
+				  Datum tupleid, Relation heapRel, IndexInfo *indexInfo)
+{
+	OTableModifyResult result;
+	ORelOids	oids;
+	OIndexType	ix_type;
+	OIndexDescr *index_descr;
+	OTableDescr *descr;
+	OIndexNumber ix_num;
+	CommitSeqNo csn;
+	OXid		oxid;
+	uint32		version;
+	TupleTableSlot *slot;
+	OTuple		tuple;
+
+	if (rel->rd_index->indisprimary)
+		return true;
+
+	ORelOidsSetFromRel(oids, rel);
+	if (rel->rd_index->indisprimary)
+		ix_type = oIndexPrimary;
+	else if (rel->rd_index->indisunique)
+		ix_type = oIndexUnique;
+	else
+		ix_type = oIndexRegular;
+	index_descr = o_fetch_index_descr(oids, ix_type, false, NULL);
+	Assert(index_descr != NULL);
+	descr = o_fetch_table_descr(index_descr->tableOids);
+	Assert(descr != NULL);
+
+	/* Find ix_num */
+	for (ix_num = 0; ix_num < descr->nIndices; ix_num++)
+	{
+		OIndexDescr *index;
+		index = descr->indices[ix_num];
+		if (index->oids.reloid == rel->rd_rel->oid)
+			break;
+	}
+	Assert(ix_num < descr->nIndices);
+
+	slot = MakeSingleTupleTableSlot(index_descr->leafTupdesc, &TTSOpsOrioleDB);
+	append_rowid_values(index_descr,
+						GET_PRIMARY(descr)->nonLeafTupdesc,
+						&GET_PRIMARY(descr)->nonLeafSpec,
+						tupleid, values, isnull,
+						&csn, &version);
+	tuple = o_form_tuple(index_descr->leafTupdesc, &index_descr->leafSpec,
+						 version, values, isnull);
+	tts_orioledb_store_tuple(slot, tuple, descr, csn, ix_num, true, NULL);
+
+	fill_current_oxid_csn(&oxid, &csn);
+
+	result = o_tbl_index_delete(index_descr, ix_num, slot, oxid, csn);
+
+	if (!result.success)
+	{
+		switch (result.action)
+		{
+			case BTreeOperationUpdate:
+				{
+					int			i;
+					StringInfo	str = makeStringInfo();
+
+					if (result.failedIxNum == PrimaryIndexNumber)
+						break;		/* it is ok */
+
+					appendStringInfo(str, "(");
+					for (i = 0; i < index_descr->nUniqueFields; i++)
+					{
+						if (i != 0)
+							appendStringInfo(str, ", ");
+						if (isnull[i])
+							appendStringInfo(str, "null");
+						else
+						{
+							Oid			typoutput;
+							bool		typisvarlena;
+							char	   *res;
+
+							getTypeOutputInfo(index_descr->nonLeafTupdesc->attrs[i].atttypid,
+											  &typoutput, &typisvarlena);
+							res = OidOutputFunctionCall(typoutput, values[i]);
+							appendStringInfo(str, "'%s'", res);
+						}
+					}
+					appendStringInfo(str, ")");
+					ereport(ERROR,
+							(errcode(ERRCODE_INTERNAL_ERROR),
+							errmsg("unable to remove tuple from secondary index in \"%s\"",
+									RelationGetRelationName(rel)),
+							errdetail("Unable to remove %s from index \"%s\"",
+									  str->data,
+									  index_descr->name.data),
+							errtableconstraint(rel, "sk")));
+					break;
+				}
+			case BTreeOperationInsert:
+				break;
+			default:
+				ereport(ERROR,
+						(errcode(ERRCODE_INTERNAL_ERROR),
+						 errmsg("Unsupported BTreeOperationType.")));
+				break;
+		}
+	}
+
+	return result.success;
+}
 
 IndexBulkDeleteResult *
 orioledb_ambulkdelete(IndexVacuumInfo *info, IndexBulkDeleteResult *stats,
