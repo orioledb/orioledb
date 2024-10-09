9ba76d4f0fe76a024d87e733edef5fc3ee07bae6
Pavel Borisov <pashkin.elfe@gmail.com>
2024-10-09 23:38:29 +0400

    SAOP changes Fix plan formatting test results

--- src/tableam/index_scan.c
+++ src/tableam/index_scan.c
@@ -183,7 +183,7 @@ switch_to_next_range(OIndexDescr *indexDescr, OScanState *ostate,
 			elog(LOG, "_bt_start_prim_scan, result %u:", result); 
 			if(result) 
 			{
-				result = _bt_advance_array_keys_increment(scan, ForwardScanDirection);
+//				result = _bt_advance_array_keys_increment(scan, ForwardScanDirection);
 				elog(LOG, "_bt_advance_array_keys_increment, result %u:", result);
 			}
 		}
@@ -196,9 +196,17 @@ switch_to_next_range(OIndexDescr *indexDescr, OScanState *ostate,
 	}
 	else
 	{
+		if (ostate->curKeyRangeIsLoaded)
+		{
 		result = false;
-//		so->needPrimScan = false;
-		elog(LOG, "no array keys");
+		so->needPrimScan = false;
+		so->scanBehind = false;
+//		elog(LOG, "no array keys");
+		}
+		else
+		{
+		result = true;
+		}
 	}
 #else
 	if (ostate->curKeyRangeIsLoaded)
