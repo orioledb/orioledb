0b95cb6f69bf9e3b3db94d84e4bc76236be1d8e5
Ilya Kobets <i.kobets@orioledb.com>
2024-11-18 10:55:07 +0100

    Added invisible field and invisible index for index bridging [skip ci]

--- test/sql/indices.sql
+++ test/sql/indices.sql
@@ -12,17 +12,17 @@ CREATE EXTENSION orioledb;
 SELECT orioledb_parallel_debug_start();
 
 -- TODO: Move test below
-set log_error_verbosity = 'terse';
 CREATE TABLE o_test_ix_ams (
 	i int NOT NULL,
 	j int4[],
 	p point
-) USING orioledb;
+) USING orioledb WITH (index_bridging);
 create index o_test_ix_ams_ix1 on o_test_ix_ams using btree (j);
 create index o_test_ix_ams_ix2 on o_test_ix_ams using hash (j);
 create index o_test_ix_ams_ix3 on o_test_ix_ams using gin (j);
 CREATE INDEX o_test_ix_ams_ix4 ON o_test_ix_ams USING gist (p);
 \d o_test_ix_ams
+SELECT orioledb_table_description('o_test_ix_ams'::regclass);
 SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass);
 RESET log_error_verbosity;
 \q
