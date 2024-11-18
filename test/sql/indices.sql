d4dcb746e23d49d30e10c05dc9aedf2577a1d303
Ilya Kobets <i.kobets@orioledb.com>
2024-11-18 14:59:41 +0100

    Added bridge index to table descriptor [skip ci]

--- test/sql/indices.sql
+++ test/sql/indices.sql
@@ -15,16 +15,19 @@ SELECT orioledb_parallel_debug_start();
 CREATE TABLE o_test_ix_ams (
 	i int NOT NULL,
 	j int4[],
-	p point
+	p point,
+	pk1 int,
+	pk2 int
 ) USING orioledb WITH (index_bridging);
 create index o_test_ix_ams_ix1 on o_test_ix_ams using btree (j);
-create index o_test_ix_ams_ix2 on o_test_ix_ams using hash (j);
-create index o_test_ix_ams_ix3 on o_test_ix_ams using gin (j);
-CREATE INDEX o_test_ix_ams_ix4 ON o_test_ix_ams USING gist (p);
+-- create index o_test_ix_ams_ix2 on o_test_ix_ams using hash (j);
+-- create index o_test_ix_ams_ix3 on o_test_ix_ams using gin (j);
+-- CREATE INDEX o_test_ix_ams_ix4 ON o_test_ix_ams USING gist (p);
 \d o_test_ix_ams
 SELECT orioledb_table_description('o_test_ix_ams'::regclass);
-SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass);
-RESET log_error_verbosity;
+SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
+ALTER TABLE o_test_ix_ams ADD PRIMARY KEY (pk2, pk1);
+SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
 \q
 
 CREATE TABLE o_test50
