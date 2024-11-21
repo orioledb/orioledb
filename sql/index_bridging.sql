1f89d573ac7c0616de80d79d1b205f2b506336ed
Ilya Kobets <i.kobets@orioledb.com>
2024-11-22 00:17:23 +0100

    Moved bridge index to begining after pkey or ctid_primary, moved bridge_ctid to begining of pkey or after ctid
    [skip ci]

--- sql/index_bridging.sql
+++ sql/index_bridging.sql
@@ -8,19 +8,33 @@ CREATE TABLE o_test_ix_ams (
 	p point,
 	pk1 int,
 	pk2 int
-) USING orioledb;
--- ) USING orioledb WITH (index_bridging);
+-- ) USING orioledb;
+) USING orioledb WITH (index_bridging);
+
+SELECT orioledb_table_description('o_test_ix_ams'::regclass);
+SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
+
 INSERT INTO o_test_ix_ams VALUES (1, ARRAY[2,3], point(4, 5), 6, 7);
+SELECT orioledb_tbl_structure('o_test_ix_ams'::regclass, 'ne');
+
+EXPLAIN (COSTS OFF)
+	SELECT * FROM o_test_ix_ams;
+SELECT * FROM o_test_ix_ams;
+
+SELECT orioledb_tbl_structure('o_test_ix_ams'::regclass, 'ne');
+
 CREATE INDEX o_test_ix_ams_ix1 on o_test_ix_ams using btree (j);
+
+SELECT orioledb_tbl_structure('o_test_ix_ams'::regclass, 'ne');
+SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
+
+ALTER TABLE o_test_ix_ams ADD PRIMARY KEY (pk2, pk1);
+SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
+SELECT orioledb_tbl_structure('o_test_ix_ams'::regclass, 'ne');
+
 -- CREATE INDEX o_test_ix_ams_ix2 ON o_test_ix_ams USING hash (j);
 -- CREATE INDEX o_test_ix_ams_ix3 ON o_test_ix_ams USING gin (j);
 -- CREATE INDEX o_test_ix_ams_ix4 ON o_test_ix_ams USING gist (p);
-\d o_test_ix_ams
-SELECT orioledb_table_description('o_test_ix_ams'::regclass);
-SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
-ALTER TABLE o_test_ix_ams ADD PRIMARY KEY (pk2, pk1);
-SELECT orioledb_tbl_indices('o_test_ix_ams'::regclass, true);
-
 DROP EXTENSION orioledb CASCADE;
 DROP SCHEMA index_bridging CASCADE;
 RESET search_path;
