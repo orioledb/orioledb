d8dcdde8fb600a6a629dd01e048c33bf65774cdc
Alexander Korotkov <akorotkov@postgresql.org>
2024-12-30 08:18:39 +0200

    Disable verify_asan_link_order asan option

--- ci/check.sh
+++ ci/check.sh
@@ -23,7 +23,7 @@ if [ $CHECK_TYPE = "valgrind_1" ]; then
 elif [ $CHECK_TYPE = "valgrind_2" ]; then
 	make USE_PGXS=1 IS_DEV=1 VALGRIND=1 testgrescheck_part_2 -j $(nproc) || status=$?
 elif [ $CHECK_TYPE = "sanitize" ]; then
-	ASAN_OPTIONS=detect_stack_use_after_return=0:detect_leaks=0:abort_on_error=1:disable_coredump=0:strict_string_checks=1:check_initialization_order=1:strict_init_order=1:detect_odr_violation=0 \
+	ASAN_OPTIONS=verify_asan_link_order=0:detect_stack_use_after_return=0:detect_leaks=0:abort_on_error=1:disable_coredump=0:strict_string_checks=1:check_initialization_order=1:strict_init_order=1:detect_odr_violation=0 \
 		make USE_PGXS=1 IS_DEV=1 installcheck -j $(nproc) || status=$?
 else
 	make USE_PGXS=1 IS_DEV=1 installcheck -j $(nproc) || status=$?
