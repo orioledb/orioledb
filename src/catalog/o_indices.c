orioledb/orioledb

commit efb3e2a58c6f1cdabfde8244a8288183508100f3

Support for bridged indexes
This commit allows the use of regular PostgreSQL indexes with OrioleDB tables.
We call these indexes "bridged" as there is a special "bridge" index, which
maps tid => pkey value.

Some important properties.

 * The bridge index assigns a new TID only when the value of at least one of
   the bridged indexes is updated.
 * Tuples are not erased in the bridge index.  They are kept for VACUUM so
   that VACUUM can clean up bridge indexes.
 * Bridge indexes have special WAL records for all their operations, including
   insert, delete, and vacuum.
 * Scans over bridge indexes first scan the bridged index, then the bridge
   index, and then the primary key.
 * As many updates of indexed values could occur in the table, there is an
   overflow protection for bridged ctid's.

Author: Ilya Kobets <i.kobets@orioledb.com>
Author: Alexander Korotkov <alexander@orioledb.com>

src/catalog/o_indices.c
@@ -180,6 +180,7 @@ make_ctid_o_index(OTable *table)
 {
 	OIndex	   *result = (OIndex *) palloc0(sizeof(OIndex));
 	int			i;
+	int			nadded = 0;
 
 	Assert(!table->has_primary);
 	result->indexOids = table->oids;
@@ -191,22 +192,28 @@ make_ctid_o_index(OTable *table)
 	result->compress = table->primary_compress;
 	result->fillfactor = table->fillfactor;
 	result->nLeafFields = table->nfields + 1;
+	if (table->index_bridging)
+		result->nLeafFields++;
 	result->nNonLeafFields = 1;
 	result->nPrimaryFields = 0;
 	result->nKeyFields = 1;
 	result->nUniqueFields = 1;
 
-	result->leafFields = (OTableField *) palloc0(sizeof(OTableField) *
-												 result->nLeafFields);
-	result->nonLeafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) *
-														 result->nNonLeafFields);
+	result->leafTableFields = (OTableField *) palloc0(sizeof(OTableField) * result->nLeafFields);
+	result->leafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) * result->nLeafFields);
 
-	make_builtin_field(&result->leafFields[0], &result->nonLeafFields[0],
+	make_builtin_field(&result->leafTableFields[nadded++], &result->leafFields[0],
 					   TIDOID, "ctid", SelfItemPointerAttributeNumber,
 					   table->tid_btree_ops_oid);
 
+	result->bridging = table->index_bridging;
+	if (table->index_bridging)
+		make_builtin_field(&result->leafTableFields[nadded++], NULL,
+						   TIDOID, "index_bridging_ctid", FirstLowInvalidHeapAttributeNumber,
+						   table->tid_btree_ops_oid);
+
 	for (i = 0; i < table->nfields; i++)
-		result->leafFields[i + 1] = table->fields[i];
+		result->leafTableFields[nadded++] = table->fields[i];
 
 	return result;
 }
@@ -219,8 +226,8 @@ find_existing_field(OIndex *index, int maxIndex, OTableIndexField *field)
 	for (i = 0; i < maxIndex; i++)
 	{
 		if (field->attnum != EXPR_ATTNUM &&
-			field->attnum == index->nonLeafFields[i].attnum &&
-			field->opclass == index->nonLeafFields[i].opclass)
+			field->attnum == index->leafFields[i].attnum &&
+			field->opclass == index->leafFields[i].opclass)
 		{
 			return i;
 		}
@@ -233,8 +240,9 @@ make_primary_o_index(OTable *table)
 {
 	OTableIndex *tableIndex;
 	OIndex	   *result = (OIndex *) palloc0(sizeof(OIndex));
-	int			i,
-				nadded = 0;
+	int			i;
+	int			saved_nLeafFields;
+	int			nadded = 0;
 	MemoryContext mcxt;
 	MemoryContext old_mcxt;
 
@@ -252,27 +260,36 @@ make_primary_o_index(OTable *table)
 	else
 		result->compress = table->primary_compress;
 	result->fillfactor = table->fillfactor;
+	saved_nLeafFields = table->nfields;
 	result->nLeafFields = table->nfields;
+	if (table->index_bridging)
+		result->nLeafFields++;
 	result->nNonLeafFields = tableIndex->nfields;
 	result->nIncludedFields = tableIndex->nfields - tableIndex->nkeyfields;
 	result->nPrimaryFields = 0;
 	result->nKeyFields = tableIndex->nkeyfields;
 
-	result->leafFields = (OTableField *) palloc0(sizeof(OTableField) *
-												 result->nLeafFields);
-	result->nonLeafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) *
-														 result->nNonLeafFields);
+	result->leafTableFields = (OTableField *) palloc0(sizeof(OTableField) * result->nLeafFields);
+	result->leafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) * result->nLeafFields);
+
+	result->bridging = table->index_bridging;
+	if (table->index_bridging)
+		make_builtin_field(&result->leafTableFields[nadded++], NULL,
+						   TIDOID, "index_bridging_ctid", FirstLowInvalidHeapAttributeNumber,
+						   table->tid_btree_ops_oid);
 
 	/*
 	 * TODO: We should probably use add_index_fields to not duplicate code,
 	 * but now it should be rewritten
 	 */
-	for (i = 0; i < result->nLeafFields; i++)
-		result->leafFields[i] = table->fields[i];
+	for (i = 0; i < saved_nLeafFields; i++)
+		result->leafTableFields[nadded++] = table->fields[i];
 
 	/* Switching context to store duplicates */
 	mcxt = OGetIndexContext(result);
 	old_mcxt = MemoryContextSwitchTo(mcxt);
+
+	nadded = 0;
 	for (i = 0; i < result->nNonLeafFields; i++)
 	{
 		int			found_attnum;
@@ -293,7 +310,7 @@ make_primary_o_index(OTable *table)
 			continue;
 		}
 
-		result->nonLeafFields[nadded++] = tableIndex->fields[i];
+		result->leafFields[nadded++] = tableIndex->fields[i];
 	}
 	MemoryContextSwitchTo(old_mcxt);
 	Assert(nadded <= result->nNonLeafFields);
@@ -343,11 +360,11 @@ add_index_fields(OIndex *index, OTable *table, OTableIndex *tableIndex, int *nad
 			}
 
 			if (attnum != EXPR_ATTNUM)
-				index->leafFields[*nadded] = table->fields[attnum];
+				index->leafTableFields[*nadded] = table->fields[attnum];
 			else
-				index->leafFields[*nadded] = tableIndex->exprfields[expr_field++];
-			Assert(index->leafFields[*nadded].typid);
-			index->nonLeafFields[*nadded] = tableIndex->fields[i];
+				index->leafTableFields[*nadded] = tableIndex->exprfields[expr_field++];
+			Assert(index->leafTableFields[*nadded].typid);
+			index->leafFields[*nadded] = tableIndex->fields[i];
 			if (fillPrimary)
 				index->primaryFieldsAttnums[index->nPrimaryFields++] = *nadded + 1;
 			(*nadded)++;
@@ -355,7 +372,7 @@ add_index_fields(OIndex *index, OTable *table, OTableIndex *tableIndex, int *nad
 	}
 	else
 	{
-		make_builtin_field(&index->leafFields[*nadded], &index->nonLeafFields[*nadded],
+		make_builtin_field(&index->leafTableFields[*nadded], &index->leafFields[*nadded],
 						   TIDOID, "ctid", SelfItemPointerAttributeNumber,
 						   table->tid_btree_ops_oid);
 		if (fillPrimary)
@@ -385,6 +402,7 @@ make_secondary_o_index(OTable *table, OTableIndex *tableIndex)
 	result->tableOids = table->oids;
 	result->table_persistence = table->persistence;
 	result->primaryIsCtid = !table->has_primary;
+	result->bridging = table->index_bridging;
 	result->compress = tableIndex->compress;
 	result->fillfactor = tableIndex->fillfactor;
 	result->nulls_not_distinct = tableIndex->nulls_not_distinct;
@@ -395,10 +413,8 @@ make_secondary_o_index(OTable *table, OTableIndex *tableIndex)
 	else
 		result->nLeafFields++;
 	result->nNonLeafFields = result->nLeafFields;
-	result->leafFields = (OTableField *) palloc0(sizeof(OTableField) *
-												 result->nLeafFields);
-	result->nonLeafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) *
-														 result->nNonLeafFields);
+	result->leafTableFields = (OTableField *) palloc0(sizeof(OTableField) * result->nLeafFields);
+	result->leafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) * result->nLeafFields);
 	result->nKeyFields = tableIndex->nkeyfields;
 
 	nadded = 0;
@@ -464,25 +480,23 @@ make_toast_o_index(OTable *table)
 	result->nLeafFields += TOAST_LEAF_FIELDS_NUM;
 	result->nNonLeafFields += TOAST_NON_LEAF_FIELDS_NUM;
 
-	result->leafFields = (OTableField *) palloc0(sizeof(OTableField) *
-												 result->nLeafFields);
-	result->nonLeafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) *
-														 result->nNonLeafFields);
+	result->leafTableFields = (OTableField *) palloc0(sizeof(OTableField) * result->nLeafFields);
+	result->leafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) * result->nLeafFields);
 
 	nadded = 0;
 	add_index_fields(result, table, primary, &nadded, true);
-	make_builtin_field(&result->leafFields[nadded], &result->nonLeafFields[nadded],
+	make_builtin_field(&result->leafTableFields[nadded], &result->leafFields[nadded],
 					   INT2OID, "attnum", FirstLowInvalidHeapAttributeNumber,
 					   INT2_BTREE_OPS_OID);
 	nadded++;
-	make_builtin_field(&result->leafFields[nadded], &result->nonLeafFields[nadded],
+	make_builtin_field(&result->leafTableFields[nadded], &result->leafFields[nadded],
 					   INT4OID, "chunknum", FirstLowInvalidHeapAttributeNumber,
 					   INT4_BTREE_OPS_OID);
 	nadded++;
 	Assert(nadded <= result->nNonLeafFields);
 	result->nUniqueFields = nadded;
 	result->nNonLeafFields = nadded;
-	make_builtin_field(&result->leafFields[nadded], NULL,
+	make_builtin_field(&result->leafTableFields[nadded], NULL,
 					   BYTEAOID, "data", FirstLowInvalidHeapAttributeNumber,
 					   InvalidOid);
 	nadded++;
@@ -492,11 +506,55 @@ make_toast_o_index(OTable *table)
 	return result;
 }
 
+static OIndex *
+make_bridge_o_index(OTable *table)
+{
+	OTableIndex *primary = NULL;
+	OIndex	   *result = (OIndex *) palloc0(sizeof(OIndex));
+	int			nadded;
+
+	if (table->has_primary)
+	{
+		primary = &table->indices[0];
+		Assert(primary->type == oIndexPrimary);
+	}
+
+	result->indexOids = table->bridge_oids;
+	result->indexType = oIndexBridge;
+	namestrcpy(&result->name, "index_bridge");
+	result->tableOids = table->oids;
+	result->bridging = true;
+	result->table_persistence = table->persistence;
+	result->primaryIsCtid = !table->has_primary;
+	result->compress = table->primary_compress;
+	result->nLeafFields = 1;
+	if (table->has_primary)
+		result->nLeafFields += primary->nfields;
+	else
+		result->nLeafFields++;
+	result->nNonLeafFields = 1;
+	result->nKeyFields = 1;
+	result->nUniqueFields = 1;
+	result->leafTableFields = (OTableField *) palloc0(sizeof(OTableField) * result->nLeafFields);
+	result->leafFields = (OTableIndexField *) palloc0(sizeof(OTableIndexField) * result->nLeafFields);
+
+	nadded = 0;
+	make_builtin_field(&result->leafTableFields[0], &result->leafFields[0],
+					   TIDOID, "index_bridging_ctid", FirstLowInvalidHeapAttributeNumber,
+					   table->tid_btree_ops_oid);
+	nadded++;
+	add_index_fields(result, table, primary, &nadded, true);
+	Assert(nadded == result->nLeafFields);
+	result->nLeafFields = nadded;
+
+	return result;
+}
+
 void
 free_o_index(OIndex *o_index)
 {
+	pfree(o_index->leafTableFields);
 	pfree(o_index->leafFields);
-	pfree(o_index->nonLeafFields);
 	if (o_index->index_mctx)
 	{
 		MemoryContextDelete(o_index->index_mctx);
@@ -575,10 +633,10 @@ serialize_o_index(OIndex *o_index, int *size)
 	appendBinaryStringInfo(&str,
 						   (Pointer) o_index + offsetof(OIndex, tableOids),
 						   offsetof(OIndex, leafFields) - offsetof(OIndex, tableOids));
+	appendBinaryStringInfo(&str, (Pointer) o_index->leafTableFields,
+						   o_index->nLeafFields * sizeof(o_index->leafTableFields[0]));
 	appendBinaryStringInfo(&str, (Pointer) o_index->leafFields,
 						   o_index->nLeafFields * sizeof(o_index->leafFields[0]));
-	appendBinaryStringInfo(&str, (Pointer) o_index->nonLeafFields,
-						   o_index->nNonLeafFields * sizeof(o_index->nonLeafFields[0]));
 	o_serialize_node((Node *) o_index->predicate, &str);
 	if (o_index->predicate)
 		o_serialize_string(o_index->predicate_str, &str);
@@ -608,15 +666,15 @@ deserialize_o_index(OIndexChunkKey *key, Pointer data, Size length)
 	ptr += len;
 
 	len = oIndex->nLeafFields * sizeof(OTableField);
-	oIndex->leafFields = (OTableField *) palloc(len);
+	oIndex->leafTableFields = (OTableField *) palloc(len);
 	Assert((ptr - data) + len <= length);
-	memcpy(oIndex->leafFields, ptr, len);
+	memcpy(oIndex->leafTableFields, ptr, len);
 	ptr += len;
 
-	len = oIndex->nNonLeafFields * sizeof(OTableIndexField);
-	oIndex->nonLeafFields = (OTableIndexField *) palloc(len);
+	len = oIndex->nLeafFields * sizeof(OTableIndexField);
+	oIndex->leafFields = (OTableIndexField *) palloc(len);
 	Assert((ptr - data) + len <= length);
-	memcpy(oIndex->nonLeafFields, ptr, len);
+	memcpy(oIndex->leafFields, ptr, len);
 	ptr += len;
 
 	mcxt = OGetIndexContext(oIndex);
@@ -650,9 +708,20 @@ make_o_index(OTable *table, OIndexNumber ixNum)
 	{
 		index = make_toast_o_index(table);
 	}
+	else if (ixNum == BridgeIndexNumber)
+	{
+		index = make_bridge_o_index(table);
+	}
 	else
 	{
-		OTableIndex *tableIndex = &table->indices[ixNum - (primaryIsCtid ? 1 : 0)];
+		OTableIndex *tableIndex;
+		int			ctid_off = 0;
+
+		if (primaryIsCtid)
+			ctid_off++;
+
+		Assert(ixNum - ctid_off >= 0);
+		tableIndex = &table->indices[ixNum - ctid_off];
 
 		index = make_secondary_o_index(table, tableIndex);
 	}
@@ -779,7 +848,7 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 	descr->refcnt = 0;
 	descr->valid = true;
 	namestrcpy(&descr->name, oIndex->name.data);
-	descr->leafTupdesc = o_table_fields_make_tupdesc(oIndex->leafFields,
+	descr->leafTupdesc = o_table_fields_make_tupdesc(oIndex->leafTableFields,
 													 oIndex->nLeafFields);
 
 	if (oIndex->indexType == oIndexPrimary)
@@ -808,7 +877,7 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 		if (oIndex->primaryIsCtid)
 		{
 			Assert(oIndex->nNonLeafFields == 1);
-			Assert(oIndex->nonLeafFields[0].attnum == SelfItemPointerAttributeNumber);
+			Assert(oIndex->leafFields[0].attnum == SelfItemPointerAttributeNumber);
 			TupleDescCopyEntry(descr->nonLeafTupdesc, 1,
 							   descr->leafTupdesc, 1);
 		}
@@ -816,7 +885,8 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 		{
 			for (i = 0; i < oIndex->nNonLeafFields; i++)
 			{
-				int			attnum = oIndex->nonLeafFields[i].attnum;
+				int			ctid_off = oIndex->bridging ? 1 : 0;
+				int			attnum = oIndex->leafFields[i].attnum + ctid_off;
 
 				Assert(attnum >= 0 && attnum < oIndex->nLeafFields);
 				TupleDescCopyEntry(descr->nonLeafTupdesc,
@@ -827,10 +897,19 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 		}
 	}
 	else if (oIndex->indexType == oIndexRegular ||
-			 oIndex->indexType == oIndexUnique)
+			 oIndex->indexType == oIndexUnique ||
+			 oIndex->indexType == oIndexBridge)
 	{
-		Assert(oIndex->nNonLeafFields == oIndex->nLeafFields);
-		descr->nonLeafTupdesc = CreateTupleDescCopy(descr->leafTupdesc);
+		if (oIndex->nNonLeafFields == oIndex->nLeafFields)
+			descr->nonLeafTupdesc = CreateTupleDescCopy(descr->leafTupdesc);
+		else
+		{
+			Assert(strncmp(oIndex->name.data, "index_bridge", NAMEDATALEN) == 0);
+			Assert(oIndex->nNonLeafFields == 1);
+			descr->nonLeafTupdesc = CreateTemplateTupleDesc(oIndex->nNonLeafFields);
+			TupleDescCopyEntry(descr->nonLeafTupdesc, 1,
+							   descr->leafTupdesc, 1);
+		}
 	}
 	else if (oIndex->indexType == oIndexToast)
 	{
@@ -841,6 +920,7 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 							   descr->leafTupdesc, i + 1);
 	}
 	descr->primaryIsCtid = oIndex->primaryIsCtid;
+	descr->bridging = oIndex->bridging;
 	descr->unique = (oIndex->indexType == oIndexUnique ||
 					 oIndex->indexType == oIndexPrimary);
 	descr->nulls_not_distinct = oIndex->nulls_not_distinct;
@@ -849,12 +929,11 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 
 	descr->nKeyFields = oIndex->nKeyFields;
 	descr->nIncludedFields = oIndex->nIncludedFields;
-	for (i = 0; i < oIndex->nNonLeafFields; i++)
+	for (i = 0; i < oIndex->nLeafFields; i++)
 	{
 		OIndexField *field = &descr->fields[i];
-		OTableIndexField *iField = &oIndex->nonLeafFields[i];
+		OTableIndexField *iField = &oIndex->leafFields[i];
 		int			attnum = iField->attnum;
-		bool		add_opclass = false;
 
 		if (attnum == SelfItemPointerAttributeNumber)
 		{
@@ -873,6 +952,14 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 
 		field->tableAttnum = attnum;
 		maxTableAttnum = Max(maxTableAttnum, attnum);
+	}
+
+	for (i = 0; i < oIndex->nNonLeafFields; i++)
+	{
+		OIndexField *field = &descr->fields[i];
+		OTableIndexField *iField = &oIndex->leafFields[i];
+		bool		add_opclass = false;
+
 		field->collation = TupleDescAttr(descr->nonLeafTupdesc, i)->attcollation;
 		if (OidIsValid(iField->collation))
 			field->collation = iField->collation;
@@ -904,7 +991,8 @@ o_index_fill_descr(OIndexDescr *descr, OIndex *oIndex, OTable *oTable)
 	{
 		descr->old_leaf_slot = MakeSingleTupleTableSlot(descr->leafTupdesc, &TTSOpsOrioleDB);
 		descr->new_leaf_slot = MakeSingleTupleTableSlot(descr->leafTupdesc, &TTSOpsOrioleDB);
-		cache_scan_tupdesc_and_slot(descr, oIndex);
+		if (oIndex->indexType != oIndexBridge)
+			cache_scan_tupdesc_and_slot(descr, oIndex);
 	}
 
 	o_set_syscache_hooks();
@@ -968,6 +1056,7 @@ o_indices_add(OTable *table, OIndexNumber ixNum, OXid oxid, CommitSeqNo csn)
 	BTreeDescr *sys_tree;
 
 	oIndex = make_o_index(table, ixNum);
+
 	oIndex->createOxid = oxid;
 	key.oids = oIndex->indexOids;
 	key.type = oIndex->indexType;
@@ -1148,6 +1237,8 @@ index_type_to_str(OIndexType type)
 			return "unique";
 		case oIndexRegular:
 			return "regular";
+		case oIndexBridge:
+			return "bridge";
 		default:
 			return "invalid";
 	}
@@ -1164,6 +1255,8 @@ index_type_from_str(const char *s, int len)
 		return oIndexUnique;
 	else if (!strncmp(s, "regular", len))
 		return oIndexRegular;
+	else if (!strncmp(s, "bridge", len))
+		return oIndexBridge;
 	else
 		return oIndexInvalid;
 }
@@ -1240,7 +1333,7 @@ describe_index(TupleDesc tupdesc, ORelOids oids, OIndexType type)
 	max_collation_str = strlen(collation_str);
 	for (i = 0; i < index->nLeafFields; i++)
 	{
-		OTableField *field = &index->leafFields[i];
+		OTableField *field = &index->leafTableFields[i];
 		char	   *typename = o_get_type_name(field->typid, field->typmod);
 		char	   *colname = get_collation_name(field->collation);
 
@@ -1270,7 +1363,7 @@ describe_index(TupleDesc tupdesc, ORelOids oids, OIndexType type)
 
 	for (i = 0; i < index->nLeafFields; i++)
 	{
-		OTableField *field = &index->leafFields[i];
+		OTableField *field = &index->leafTableFields[i];
 		char	   *typename = o_get_type_name(field->typid, field->typmod);
 		char	   *colname = get_collation_name(field->collation);
 
@@ -1285,7 +1378,7 @@ describe_index(TupleDesc tupdesc, ORelOids oids, OIndexType type)
 	appendStringInfo(&buf, "\nKey fields: (");
 	for (i = 0; i < index->nNonLeafFields; i++)
 	{
-		OTableIndexField *nonLeafField = &index->nonLeafFields[i];
+		OTableIndexField *nonLeafField = &index->leafFields[i];
 		OTableField *leafField;
 
 		if (type == oIndexPrimary)
@@ -1293,16 +1386,16 @@ describe_index(TupleDesc tupdesc, ORelOids oids, OIndexType type)
 			if (nonLeafField->attnum == SelfItemPointerAttributeNumber)
 			{
 				Assert(index->primaryIsCtid);
-				leafField = &index->leafFields[0];
+				leafField = &index->leafTableFields[0];
 			}
 			if (index->primaryIsCtid)
-				leafField = &index->leafFields[nonLeafField->attnum + 1];
+				leafField = &index->leafTableFields[nonLeafField->attnum + 1];
 			else
-				leafField = &index->leafFields[nonLeafField->attnum];
+				leafField = &index->leafTableFields[nonLeafField->attnum];
 		}
 		else
 		{
-			leafField = &index->leafFields[i];
+			leafField = &index->leafTableFields[i];
 		}
 		if (i != 0)
 			appendStringInfo(&buf, ", ");

commit 6ff9293e1c5b1a06b6017b52ffc2deb4e1fb5a85

OIndex deserialize and serialize bugfix
The bug was introduced by efb3e2a, which changed fields of OIndex. Now
leafFields is after leafTableFields. `deserialize_o_index()` and
`serialize_o_index()` weren't adjusted accordingly.

src/catalog/o_indices.c
@@ -632,7 +632,7 @@ serialize_o_index(OIndex *o_index, int *size)
 	initStringInfo(&str);
 	appendBinaryStringInfo(&str,
 						   (Pointer) o_index + offsetof(OIndex, tableOids),
-						   offsetof(OIndex, leafFields) - offsetof(OIndex, tableOids));
+						   offsetof(OIndex, leafTableFields) - offsetof(OIndex, tableOids));
 	appendBinaryStringInfo(&str, (Pointer) o_index->leafTableFields,
 						   o_index->nLeafFields * sizeof(o_index->leafTableFields[0]));
 	appendBinaryStringInfo(&str, (Pointer) o_index->leafFields,
@@ -660,7 +660,7 @@ deserialize_o_index(OIndexChunkKey *key, Pointer data, Size length)
 	oIndex->indexOids = key->oids;
 	oIndex->indexType = key->type;
 
-	len = offsetof(OIndex, leafFields) - offsetof(OIndex, tableOids);
+	len = offsetof(OIndex, leafTableFields) - offsetof(OIndex, tableOids);
 	Assert((ptr - data) + len <= length);
 	memcpy((Pointer) oIndex + offsetof(OIndex, tableOids), ptr, len);
 	ptr += len;

test/expected/btree_sys_check.out
@@ -121,19 +121,19 @@ SELECT regexp_replace(
 		'g');
                                         regexp_replace                                         
 -----------------------------------------------------------------------------------------------
- Page 0: level = 0, maxKeyLen = 24, nVacatedBytes = 4832                                      +
+ Page 0: level = 0, maxKeyLen = 24, nVacatedBytes = 4784                                      +
  state = free, datoid equal, relnode equal, ix_type = primary, clean                          +
      Leftmost, Rightmost                                                                      +
    Chunk 0: offset = 0, location = 256, hikey location = 72, hikey = (1, (NNN, NNN, NNN), 0)  +
-     Item 0: deleted, offset = 264, tuple = ((1, (NNN, NNN, NNN), 0), 665)                    +
-   Chunk 1: offset = 1, location = 976, hikey location = 96, hikey = (3, (NNN, NNN, NNN), 0)  +
-     Item 1: deleted, offset = 984, tuple = ((1, (NNN, NNN, NNN), 0), 1001)                   +
-     Item 2: deleted, offset = 2032, tuple = ((1, (NNN, NNN, NNN), 0), 665)                   +
-   Chunk 2: offset = 3, location = 2744, hikey location = 120, hikey = (3, (NNN, NNN, NNN), 0)+
-     Item 3: deleted, offset = 2752, tuple = ((3, (NNN, NNN, NNN), 0), 777)                   +
-   Chunk 3: offset = 4, location = 3576, hikey location = 144                                 +
-     Item 4: deleted, offset = 3584, tuple = ((3, (NNN, NNN, NNN), 0), 665)                   +
-     Item 5: deleted, offset = 4296, tuple = ((3, (NNN, NNN, NNN), 0), 777)                   +
+     Item 0: deleted, offset = 264, tuple = ((1, (NNN, NNN, NNN), 0), 657)                    +
+   Chunk 1: offset = 1, location = 968, hikey location = 96, hikey = (3, (NNN, NNN, NNN), 0)  +
+     Item 1: deleted, offset = 976, tuple = ((1, (NNN, NNN, NNN), 0), 993)                    +
+     Item 2: deleted, offset = 2016, tuple = ((1, (NNN, NNN, NNN), 0), 657)                   +
+   Chunk 2: offset = 3, location = 2720, hikey location = 120, hikey = (3, (NNN, NNN, NNN), 0)+
+     Item 3: deleted, offset = 2728, tuple = ((3, (NNN, NNN, NNN), 0), 769)                   +
+   Chunk 3: offset = 4, location = 3544, hikey location = 144                                 +
+     Item 4: deleted, offset = 3552, tuple = ((3, (NNN, NNN, NNN), 0), 657)                   +
+     Item 5: deleted, offset = 4256, tuple = ((3, (NNN, NNN, NNN), 0), 769)                   +
                                                                                               +
  
 (1 row)

test/expected/btree_sys_check_1.out
@@ -121,19 +121,19 @@ SELECT regexp_replace(
 		'g');
                                         regexp_replace                                         
 -----------------------------------------------------------------------------------------------
- Page 0: level = 0, maxKeyLen = 24, nVacatedBytes = 4832                                      +
+ Page 0: level = 0, maxKeyLen = 24, nVacatedBytes = 4784                                      +
  state = free, datoid equal, relnode equal, ix_type = primary, clean                          +
      Leftmost, Rightmost                                                                      +
    Chunk 0: offset = 0, location = 256, hikey location = 72, hikey = (1, (NNN, NNN, NNN), 0)  +
-     Item 0: deleted, offset = 264, tuple = ((1, (NNN, NNN, NNN), 0), 665)                    +
-   Chunk 1: offset = 1, location = 976, hikey location = 96, hikey = (3, (NNN, NNN, NNN), 0)  +
-     Item 1: deleted, offset = 984, tuple = ((1, (NNN, NNN, NNN), 0), 1001)                   +
-     Item 2: deleted, offset = 2032, tuple = ((1, (NNN, NNN, NNN), 0), 665)                   +
-   Chunk 2: offset = 3, location = 2744, hikey location = 120, hikey = (3, (NNN, NNN, NNN), 0)+
-     Item 3: deleted, offset = 2752, tuple = ((3, (NNN, NNN, NNN), 0), 777)                   +
-   Chunk 3: offset = 4, location = 3576, hikey location = 144                                 +
-     Item 4: deleted, offset = 3584, tuple = ((3, (NNN, NNN, NNN), 0), 665)                   +
-     Item 5: deleted, offset = 4296, tuple = ((3, (NNN, NNN, NNN), 0), 777)                   +
+     Item 0: deleted, offset = 264, tuple = ((1, (NNN, NNN, NNN), 0), 657)                    +
+   Chunk 1: offset = 1, location = 968, hikey location = 96, hikey = (3, (NNN, NNN, NNN), 0)  +
+     Item 1: deleted, offset = 976, tuple = ((1, (NNN, NNN, NNN), 0), 993)                    +
+     Item 2: deleted, offset = 2016, tuple = ((1, (NNN, NNN, NNN), 0), 657)                   +
+   Chunk 2: offset = 3, location = 2720, hikey location = 120, hikey = (3, (NNN, NNN, NNN), 0)+
+     Item 3: deleted, offset = 2728, tuple = ((3, (NNN, NNN, NNN), 0), 769)                   +
+   Chunk 3: offset = 4, location = 3544, hikey location = 144                                 +
+     Item 4: deleted, offset = 3552, tuple = ((3, (NNN, NNN, NNN), 0), 657)                   +
+     Item 5: deleted, offset = 4256, tuple = ((3, (NNN, NNN, NNN), 0), 769)                   +
                                                                                               +
  
 (1 row)