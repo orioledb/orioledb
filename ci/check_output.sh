a54ae37c9edd53520e3430fe326d28e449286eb1
Egor Ivkov <e.o.ivkov@gmail.com>
2025-04-25 14:55:46 +0300

    Enable running postgresql regression tests on Orioledb tables in CI

--- ci/check_output.sh
+++ ci/check_output.sh
@@ -15,7 +15,7 @@ if [ $oomcount != $oomsbefore ]; then
 fi
 
 # show diff if it exists
-for f in ` find ./orioledb/test -name regression.diffs ` ; do
+for f in ` find ./orioledb/test ./pgsql/test -name regression.diffs ` ; do
 	echo "========= Contents of $f"
 	cat $f
 	status=1
