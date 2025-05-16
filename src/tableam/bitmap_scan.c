dabe465df0e9b2657ea5fd9d28306233d7b8e118
Ilya Kobets <i.kobets@orioledb.com>
2025-05-17 01:58:34 +0200

    Do rescan if needed for bitmap scan of bridged indices
    Fixes segfault in "rangetypes" test

--- src/tableam/bitmap_scan.c
+++ src/tableam/bitmap_scan.c
@@ -360,10 +360,29 @@ o_exec_bitmapqual(OBitmapHeapPlanState *bitmap_state, PlanState *planstate)
 				BitmapIndexScanState *node;
 				Instrumentation *instrument;
 				OBTOptions *options;
+				ExprContext *econtext = bitmap_state->scan->ss->ps.ps_ExprContext;
 
 				node = (BitmapIndexScanState *) planstate;
 				instrument = node->ss.ps.instrument;
 				options = (OBTOptions *) node->biss_RelationDesc->rd_options;
+
+				if (node->biss_NumRuntimeKeys != 0)
+					ExecIndexEvalRuntimeKeys(econtext,
+											node->biss_RuntimeKeys,
+											node->biss_NumRuntimeKeys);
+				if (node->biss_NumArrayKeys != 0)
+					node->biss_RuntimeKeysReady =
+						ExecIndexEvalArrayKeys(econtext,
+											node->biss_ArrayKeys,
+											node->biss_NumArrayKeys);
+				else
+					node->biss_RuntimeKeysReady = true;
+
+				/* reset index scan */
+				if (node->biss_RuntimeKeysReady)
+					index_rescan(node->biss_ScanDesc,
+								node->biss_ScanKeys, node->biss_NumScanKeys,
+								NULL, 0);
 
 				if (instrument)
 					InstrStartNode(instrument);
