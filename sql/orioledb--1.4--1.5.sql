76b38a0c2eaab0d1d89d54c3481ae6b1520d5e5c
Alexander Korotkov <akorotkov@postgresql.org>
2025-05-21 03:30:04 +0300

    Missing sql/orioledb--1.4--1.5.sql

---
+++ sql/orioledb--1.4--1.5.sql
@@ -0,0 +1,9 @@
+/* contrib/orioledb/sql/orioledb--1.4--1.5.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION orioledb UPDATE TO '1.5'" to load this file. \quit
+
+CREATE FUNCTION orioledb_rewind_sync()
+RETURNS void
+AS 'MODULE_PATHNAME'
+VOLATILE LANGUAGE C;
