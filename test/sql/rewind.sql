4dc53c5417ffc95ced51930c2ef2575660308976
Pavel Borisov <pashkin.elfe@gmail.com>
2025-05-23 16:57:48 +0400

    Remove actual rewind test from regression as it restarts cluster. Leave checking bailout conditions

--- test/sql/rewind.sql
+++ test/sql/rewind.sql
@@ -4,26 +4,31 @@ CREATE EXTENSION orioledb;
 \set VERBOSITY terse
 \set VERBOSITY default
 
+CREATE table heap_table (i int) USING heap;
 CREATE TABLE o_test_rewind (i int) USING orioledb;
 
-INSERT INTO o_test_rewind VALUES (1);
-SELECT pg_sleep(1);
-INSERT INTO o_test_rewind VALUES (2);
-SELECT pg_sleep(1);
-INSERT INTO o_test_rewind VALUES (3);
-SELECT pg_sleep(1);
-INSERT INTO o_test_rewind VALUES (4);
-SELECT pg_sleep(1);
-INSERT INTO o_test_rewind VALUES (5);
-SELECT pg_sleep(5);
+INSERT INTO heap_table VALUES (1);
 
-select * from o_test_rewind;
+INSERT INTO o_test_rewind VALUES (1);
+INSERT INTO o_test_rewind VALUES (2);
+INSERT INTO o_test_rewind VALUES (3);
+INSERT INTO o_test_rewind VALUES (4);
+INSERT INTO o_test_rewind VALUES (5);
+
+-- Should fail
 select orioledb_rewind(600);
 select orioledb_rewind(0);
-select * from o_test_rewind;
-select orioledb_rewind(10);
-select * from o_test_rewind;
 
+BEGIN;
+INSERT INTO heap_table VALUES (2);
+PREPARE TRANSACTION 'prep';
+ROLLBACK;
+
+select orioledb_rewind(10);
+
+COMMIT PREPARED 'prep';
+
+DROP TABLE heap_table;
 DROP TABLE o_test_rewind;
 
 DROP EXTENSION orioledb CASCADE;
