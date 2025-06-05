5848d25cc58b3778ebf7b9c351f1bcaef35f27de
Pavel Borisov <pashkin.elfe@gmail.com>
2025-06-06 00:00:21 +0400

    Increase delay in tests more

--- test/t/rewind_xid_test.py
+++ test/t/rewind_xid_test.py
@@ -323,7 +323,7 @@ class RewindXidTest(BaseTest):
 		node.safe_psql('postgres',
 		               "select orioledb_rewind_to_xid(%d,%ld);\n" % (xid,oxid))
 
-		time.sleep(10)
+		time.sleep(50)
 		node.is_started = False
 		node.start()
 
