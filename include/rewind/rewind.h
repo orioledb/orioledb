606a0595d50ed55081095f0cff942c10084fbf22
Pavel Borisov <pashkin.elfe@gmail.com>
2025-07-03 18:31:42 +0400

    [skip ci] Add comments for internals of rewind

--- include/rewind/rewind.h
+++ include/rewind/rewind.h
@@ -94,6 +94,86 @@ typedef struct
 
 } RewindMeta;
 
+/*
+ * Queue of rewind items internals:
+ * --------------------------------
+ *                                                                                               F R
+ *  |      C+++++++R++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++E++++++++++++A.A       |
+ *  C - completePos, R-restorePos, E-evictPos, AF - addPosFilled, AR - addPosReserved
+ *  " " - empty items, "+" - busy items, "." - transient items
+ *  All positions are uint64 and can only increase.
+ *  E and R are modified only under lock.
+ *  C can only be increased by the only rewind worker and is not locked.
+ *  A(F) and A(R) - are atomic uint64 as they are accessed concurrently by adding backends at commit transaction.
+ *
+ *  All readable items are from C to AF. Between AF and AR some items are transiently not filled, so they
+ *   are not readable by evict/restore/complete/rewind. But up to A(R) position they are is considered "busy"
+ *   and A(R) is used for calculation of free space for eviction.
+ *
+ * Tail and head of this queue are placed into in-memory buffers completeBuffer and addBuffer.
+ * They use modulo placement so every time ( R-C < buffer_length ) and ( AR - E < buffer_length).
+ *
+ *     |      C+++++R      |        or alternatively         |++++R      C+++++|
+ *
+ *                  F R                                            F R
+ *       |      E+++A.A      |      or alternatively           |+++A.A      E+++++|
+ *
+ * Positions between R and E are written to disk (in full blocks only).
+ *
+ * There are following actions on a queue:
+ *  - Adding: to addBuffer, done by committing backends
+ *  - Eviction: from addBuffer, done by committing backends
+ *  - Restore: to completeBuffer, done by a rewind worker
+ *  - Rewind: from all buffers, done by a backend where rewind is requested. All other backends are shut down
+ *  before actual Rewind starts.
+ *  - Complete: from completeBuffer, done by a rewind worker
+ *
+ *  Adding to addBuffer:
+ *  (1) checks is the space enough, if not calls eviction first
+ *  (2) reserves space to addind by incrementing AR 
+ *  (3) adds items to the reserved space:
+ *
+ * Rewind queue items has two types
+ *  - rewindItem: mandatory for each transaction commit
+ *  - subxidsItem: optional, when subxids exist for a transaction.
+ *
+ * All items for the same transaction are written together in the following order:
+ *  rewindItem->subxidsItem(1)->...->subxidsItem(n). This is crucial for proper backwards reading during Rewind.
+ *
+ *  (4) pushes AF:
+ *   - if all concurrent backends that incremented AR finished
+ *   - if transient difference AR-AF becomes big (then transaction waits concurrent ones to complete) 
+ *
+ * Eviction from addBuffer has two ways:
+ * - fast: direct transfer from addBuffer to completeBuffer (in there is space for this)
+ * - ordinary: items written to disk and cleared from addBuffer (by full blocks)
+ *
+ * Restore to completeBuffer also has two ways:
+ * - ordinary: if some items are evicted to disk but not restored yet. So they are read from disk to free
+ *  space in conpleteBuffer (by full blocks);
+ * - fast: - if items are only in in-memory buffers. So they are directly transferred from addBuffer to
+ *  completeBuffer.
+ *
+ * At Rewind, backend reads all existing items from both in-memory buffers and on-disk buffer in reverse
+ * so from AF to C. Only one backend remains at the time of do_rewind() starts so it works only serially and doesn't
+ * need locks. RewindWorker applies undoChain from each item and restores global variables relevant to heap
+ * transaction visibility. Rewind stops (1) on reaching specified xid/oxid pair or time values (2) on reaching
+ * completePos, so maximum depth of rewind is limited by a rewind buffer.
+ *
+ * At Complete, rewind worker fix both Oriole and heap transactions remembered in queue rewindItems from C to AF.
+ * This is done serially but may require restore action when completeBuffer frees enough. Restore takes evictLock
+ * to avoid concurrent eviction by backends.
+ *
+ * Eviction has two locks:
+ * 	rewindEvictLock - against concurrent eviction (1) and eviction during restore (2). Only backend that got
+ * 	eviction lock does eviction, others skip and do not wait.
+ *
+ * 	rewindCheckpointLock - against eviction during checkpoint. All backends that couldn't get it wait until
+ * 	checkpoint releases the lock.
+ *
+ *
+ */
+
 #define InvalidRewindPos UINT64_MAX
 
 #endif							/* __REWIND_WORKER_H__ */
