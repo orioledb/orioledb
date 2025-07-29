7bcbe9faf5de23b6e1fd99a073badd64f877bfb9
Alexander Korotkov <akorotkov@postgresql.org>
2025-07-29 16:27:27 +0300

    Make regression and isolation tests pass

--- src/btree/build.c
+++ src/btree/build.c
@@ -82,7 +82,7 @@ stack_page_split(BTreeDescr *desc, OIndexBuildStackItem *stack, int level,
 					 tupleheader, tuple, tuplesize, false,
 					 COMMITSEQNO_INPROGRESS);
 
-	left_count = btree_page_split_location(desc, &items, 0, 0.9, NULL);
+	left_count = btree_page_split_location(desc, &items, offset, 0.9, NULL);
 
 	/* Distribute the tuples according the the split location */
 	BTREE_PAGE_OFFSET_GET_LOCATOR(img, left_count, &loc);
