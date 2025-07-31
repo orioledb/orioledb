b875fb0f33d6ae9bea040bbf54ff10d05ed077bd
Alexander Korotkov <akorotkov@postgresql.org>
2025-07-31 16:17:21 +0300

    Fix valgrind complaint

--- src/s3/checkpoint.c
+++ src/s3/checkpoint.c
@@ -55,6 +55,7 @@
 #include "storage/reinit.h"
 #include "utils/builtins.h"
 #include "utils/guc.h"
+#include "utils/memdebug.h"
 #include "utils/ps_status.h"
 #include "utils/relcache.h"
 #include "utils/resowner.h"
@@ -477,6 +478,7 @@ s3_backup_scan_dir(S3BackupState *state, const char *path,
 		}
 
 		snprintf(pathbuf, sizeof(pathbuf), "%s/%s", path, de->d_name);
+		VALGRIND_MAKE_MEM_DEFINED(pathbuf, sizeof(pathbuf));
 
 		/* Skip pg_control here to back up it last */
 		if (strcmp(pathbuf, "./global/pg_control") == 0)
