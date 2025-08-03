c1c2851d689bb190aef0e3fec2687e3c79d1d375
Ilya Kobets <i.kobets@orioledb.com>
2025-08-03 21:33:17 +0200

    Do not clean o_saved_relrewrite until the end of refresh mat view
    Fixes segfaults in "privileges" pg regress test

--- src/catalog/ddl.c
+++ src/catalog/ddl.c
@@ -110,6 +110,7 @@ Oid			o_saved_relrewrite = InvalidOid;
 Oid			o_saved_reltablespace = InvalidOid;
 static ORelOids saved_oids;
 static bool in_rewrite = false;
+static bool in_refresh_mat_view = false;
 List	   *reindex_list = NIL;
 Query	   *savedDataQuery = NULL;
 IndexBuildResult o_pkey_result = {0};
@@ -878,7 +879,8 @@ orioledb_utility_command(PlannedStmt *pstmt,
 		pstmt = copyObject(pstmt);
 
 	in_rewrite = false;
-	o_saved_relrewrite = InvalidOid;
+	if (!in_refresh_mat_view && OidIsValid(o_saved_relrewrite))
+		o_saved_relrewrite = InvalidOid;
 	o_saved_reltablespace = InvalidOid;
 	savedDataQuery = NULL;
 	in_nontransactional_truncate = false;
@@ -1297,6 +1299,11 @@ orioledb_utility_command(PlannedStmt *pstmt,
 		if (matviewRel->rd_rel->relkind == RELKIND_MATVIEW &&
 			is_orioledb_rel(matviewRel))
 		{
+			if (stmt->concurrent)
+			{
+				elog(WARNING, "Concurrent REFRESH MATERIALIZED VIEW not implemented for orioledb materialized vies yet, using simple one");
+				stmt->concurrent = false;
+			}
 			if (!stmt->skipData)
 			{
 				savedDataQuery = linitial_node(Query, matviewRel->rd_rules->rules[0]->actions);
@@ -1314,6 +1321,7 @@ orioledb_utility_command(PlannedStmt *pstmt,
 				}
 			}
 			stmt->skipData = true;
+			in_refresh_mat_view = true;
 		}
 		table_close(matviewRel, AccessShareLock);
 	}
@@ -1480,7 +1488,10 @@ orioledb_utility_command(PlannedStmt *pstmt,
 			alter_type_exprs = NIL;
 		}
 	}
-
+	else if (IsA(pstmt->utilityStmt, RefreshMatViewStmt))
+	{
+		in_refresh_mat_view = false;
+	}
 	free_parsestate(pstate);
 }
 
@@ -4088,7 +4099,9 @@ o_ddl_cleanup(void)
 		reindex_list = NIL;
 	}
 	memset(&o_pkey_result, 0, sizeof(o_pkey_result));
-	o_saved_relrewrite = InvalidOid;
+	if (!in_refresh_mat_view)
+		o_saved_relrewrite = InvalidOid;
+	in_refresh_mat_view = false;
 	in_rewrite = false;
 	o_in_add_column = false;
 }
