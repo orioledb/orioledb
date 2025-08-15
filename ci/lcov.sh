1b4f49ae001da079d4a8c8bd887b988f2a229ec4
Ilya Kobets <i.kobets@orioledb.com>
2025-08-15 10:01:36 +0200

    fix for lcov "Unexpected negative count" error

--- ci/lcov.sh
+++ ci/lcov.sh
@@ -5,8 +5,8 @@ set -eu
 cd orioledb
 if [ $COMPILER = "clang" ]; then
     # trick with gcov-tool needed to not have gcov version mismatch: file.gcno:version '...*', prefer '...*'
-    lcov --gcov-tool "$PWD/ci/llvm-gcov.sh" --capture --directory . --no-external --output-file coverage.info
+    lcov --gcov-tool "$PWD/ci/llvm-gcov.sh" --capture --directory . --no-external --rc geninfo_unexecuted_blocks=1 --output-file coverage.info
 else
-	lcov --capture --directory . --no-external --output-file coverage.info
+	lcov --capture --directory . --no-external --rc geninfo_unexecuted_blocks=1 --output-file coverage.info
 fi
 cd ..
