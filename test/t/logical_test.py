d23d58475bfbb7c88da48bc61498ad4b4a91c32b
Pavel Borisov <pashkin.elfe@gmail.com>
2025-09-19 03:28:38 +0400

    Add test for #553

--- test/t/logical_test.py
+++ test/t/logical_test.py
@@ -204,6 +204,52 @@ class LogicalTest(BaseTest):
 					    subscriber.execute(
 					        'SELECT * FROM o_test2 ORDER BY id'), [])
 
+	def test_logical_subscription_ctid(self):
+		with self.node as publisher:
+			publisher.start()
+
+			baseDir = mkdtemp(prefix=self.myName + '_tgsb_')
+			subscriber = testgres.get_new_node('subscriber',
+			                                   port=self.getBasePort() + 1,
+			                                   base_dir=baseDir)
+			subscriber.init(["--no-locale", "--encoding=UTF8"])
+			subscriber.append_conf(shared_preload_libraries='orioledb')
+			subscriber.append_conf(wal_level='logical')
+
+			with subscriber.start() as subscriber:
+				create_sql = """
+					CREATE EXTENSION IF NOT EXISTS orioledb;
+					CREATE TABLE o_test1 (
+						data1 text,
+						data2 text,
+			                        data3 text,
+			                        i   int
+					) USING orioledb;
+				"""
+				publisher.safe_psql(create_sql)
+				subscriber.safe_psql(create_sql)
+
+				pub = publisher.publish('test_pub', tables=['o_test1'])
+				sub = subscriber.subscribe(pub, 'test_sub')
+
+				with publisher.connect() as con1:
+					with publisher.connect() as con2:
+						con1.execute("INSERT INTO o_test1 VALUES('foofoo','barbar', 'aaaaaa', 1);")
+						con2.execute("INSERT INTO o_test1 VALUES('mmm','nnn', 'ooo', 2);")
+						con1.commit()
+						con2.commit()
+
+					self.assertListEqual(
+					    publisher.execute('SELECT * FROM o_test1 ORDER BY i'),
+					    [('foofoo','barbar', 'aaaaaa', 1), ('mmm','nnn', 'ooo', 2)])
+
+					# wait until changes apply on subscriber and check them
+					sub.catchup()
+					# sub.poll_query_until("SELECT orioledb_recovery_synchronized();", expected=True)
+					self.assertListEqual(
+					    subscriber.execute(
+					        'SELECT * FROM o_test1 ORDER BY i'), [('foofoo','barbar', 'aaaaaa', 1), ('mmm','nnn', 'ooo', 2)])
+
 	def test_recvlogical_and_drop_database(self):
 		node = self.node
 		node.start()
