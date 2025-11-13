f6fc57ee027928a0b223edd9b2e302e32f472564
GremSnoort <grem_snoort@protonmail.com>
2025-11-13 12:40:10 +0300

    Rename sql/orioledb--1.5--1.6.sql to sql/orioledb--1.5--1.6_prod.sql

--- sql/orioledb--1.5--1.6_prod.sql
+++ sql/orioledb--1.5--1.6_prod.sql
@@ -2,3 +2,8 @@
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION orioledb UPDATE TO '1.6'" to load this file. \quit
+
+CREATE FUNCTION pg_stopevent_set(eventname text, condition jsonpath, flags text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
