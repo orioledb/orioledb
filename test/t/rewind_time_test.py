2dd00d3532029e9e24101f6dea745b62d17eed6b
Nick Babadzhanian <pgnickb@github.com>
2025-11-25 17:57:28 +0100

    Restart PostgreSQL instance when rewind is done
    - Add a function that attempts to restart the instance or bails and shuts it down on failure
    - Adjust and stabilize tests to better handle this new behavior
    - Refuse to set enable_rewind on systems where clean restart is not supported
    - Make path to .supp and .log files absolute for valgrind tests (this breaks valgrind vs execl, see https://bugs.kde.org/show_bug.cgi?id=513403)

--- test/t/rewind_time_test.py
+++ test/t/rewind_time_test.py
@@ -20,6 +20,12 @@ class RewindTest(BaseTest):
 	def wait_shutdown_and_start(self, node):
 		super().wait_shutdown_and_start(node)
 
+	def wait_restart(self, node, previous_start_time):
+		super().wait_restart(node, previous_start_time)
+
+	def get_pg_start_time(self, node):
+		return super().get_pg_start_time(node)
+
 	def test_rewind_oriole(self):
 		node = self.node
 		node.append_conf(
@@ -42,6 +48,14 @@ class RewindTest(BaseTest):
 			    'postgres', "INSERT INTO o_test\n"
 			    "	VALUES (%d, %d || 'val');\n" % (i, i))
 
+		# Store the current timestamp in a temp table to reuse it later
+		node.safe_psql(
+		    'postgres', "DROP TABLE IF EXISTS o_rewind;\n"
+		    "CREATE TABLE o_rewind(ts, note) AS\n"
+		    "	SELECT\n"
+		    "		clock_timestamp() as ts,\n"
+		    "		'before sleep' as note;")
+
 		time.sleep(10)
 
 		for i in range(6, 20):
@@ -49,9 +63,17 @@ class RewindTest(BaseTest):
 			    'postgres', "INSERT INTO o_test\n"
 			    "	VALUES (%d, %d || 'val');\n" % (i, i))
 
-		node.safe_psql('postgres', "select orioledb_rewind_by_time(9);\n")
+		previous_start_time = self.get_pg_start_time(node)
 
-		self.wait_shutdown_and_start(node)
+		# Rewind to the time we stored above + 5 seconds for safety
+		node.safe_psql(
+		    'postgres', "SELECT orioledb_rewind_by_time(i-5) "
+		    "	FROM "
+		    "		o_rewind as r, "
+		    "		lateral (select floor(date_part('epoch', clock_timestamp()-r.ts))::int4) f(i) "
+		    "	WHERE note = 'before sleep';")
+
+		self.wait_restart(node, previous_start_time)
 
 		self.assertEqual(
 		    str(node.execute('postgres', 'SELECT * FROM o_test;')),
@@ -81,6 +103,14 @@ class RewindTest(BaseTest):
 			    'postgres', "INSERT INTO o_test_heap\n"
 			    "	VALUES (%d, %d || 'val');\n" % (i, i))
 
+		# Store the current timestamp in a temp table to reuse it later
+		node.safe_psql(
+		    'postgres', "DROP TABLE IF EXISTS o_rewind;\n"
+		    "CREATE TABLE o_rewind(ts, note) AS\n"
+		    "	SELECT\n"
+		    "		clock_timestamp() as ts,\n"
+		    "		'before sleep' as note;")
+
 		time.sleep(10)
 
 		for i in range(6, 20):
@@ -88,9 +118,17 @@ class RewindTest(BaseTest):
 			    'postgres', "INSERT INTO o_test_heap\n"
 			    "	VALUES (%d, %d || 'val');\n" % (i, i))
 
-		node.safe_psql('postgres', "select orioledb_rewind_by_time(9);\n")
+		previous_start_time = self.get_pg_start_time(node)
 
-		self.wait_shutdown_and_start(node)
+		# Rewind to the time we stored above + 5 seconds for safety
+		node.safe_psql(
+		    'postgres', "SELECT orioledb_rewind_by_time(i-5) "
+		    "	FROM "
+		    "		o_rewind as r, "
+		    "		lateral (select floor(date_part('epoch', clock_timestamp()-r.ts))::int4) f(i) "
+		    "	WHERE note = 'before sleep';")
+
+		self.wait_restart(node, previous_start_time)
 
 		self.assertEqual(
 		    str(node.execute('postgres', 'SELECT * FROM o_test_heap;')),
@@ -267,6 +305,11 @@ class RewindTest(BaseTest):
 		    "INSERT INTO o_test VALUES (%d, %d || 'val'); SAVEPOINT sp1;\n"
 		    "INSERT INTO o_test_heap VALUES (%d, %d || 'val');\n"
 		    "INSERT INTO o_test VALUES (%d, %d || 'val'); SAVEPOINT sp2;\n"
+		    "DROP TABLE IF EXISTS o_rewind;"
+		    "CREATE TABLE o_rewind(ts, note) AS\n"
+		    "	SELECT\n"
+		    "		clock_timestamp() as ts,\n"
+		    "		'before sleep' as note;"
 		    "SELECT pg_sleep(10);\n"
 		    "INSERT INTO o_test_heap VALUES (%d, %d || 'val');\n"
 		    "INSERT INTO o_test VALUES (%d, %d || 'val'); SAVEPOINT sp3;\n"
@@ -289,9 +332,15 @@ class RewindTest(BaseTest):
 			    (i, i, i, i, i + 1, i + 1, i + 1, i + 1, i + 2, i + 2, i + 2,
 			     i + 2, i + 3, i + 3, i + 3, i + 3))
 
-		node.safe_psql('postgres', "select orioledb_rewind_by_time(9);\n")
+		previous_start_time = self.get_pg_start_time(node)
+		node.safe_psql(
+		    'postgres', "SELECT orioledb_rewind_by_time(i-5) "
+		    "	FROM "
+		    "		o_rewind as r, "
+		    "		lateral (select floor(date_part('epoch', clock_timestamp()-r.ts))::int4) f(i) "
+		    "	WHERE note = 'before sleep';")
 
-		self.wait_shutdown_and_start(node)
+		self.wait_restart(node, previous_start_time)
 
 		self.maxDiff = None
 		self.assertEqual(
