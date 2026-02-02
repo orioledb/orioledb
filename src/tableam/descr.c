b2b8c7ff6d98c4f3c6c3f524a4b3e660b169f0d2
Pavel Borisov <pashkin.elfe@gmail.com>
2026-02-03 00:52:03 +0400

    Revert "Remove useless rootInfo assignment"
    This reverts commit 90bd8527a8cebf515921b727afd6bd2a4706b775.

--- src/tableam/descr.c
+++ src/tableam/descr.c
@@ -326,6 +326,7 @@ o_btree_load_shmem_internal(BTreeDescr *desc, bool checkpoint)
 			evictable_tree_init(desc, true, &was_evicted);
 		}
 		is_compressed = OCompressIsValid(desc->compress);
+		desc->rootInfo = sharedRootInfo->rootInfo;
 
 		init_extents = false;
 		if (is_compressed && !was_evicted)
