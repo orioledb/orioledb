b6059557154a12b5acc7bd0ecbfc05d0c9527371
Trunov Vladimir <trunov.vv@phystech.edu>
2026-02-11 15:41:30 +0300

    support for at_setexpression added

--- src/catalog/ddl.c
+++ src/catalog/ddl.c
@@ -108,6 +108,7 @@ static object_access_hook_type old_objectaccess_hook = NULL;
 List	   *drop_index_list = NIL;
 List	   *partition_drop_index_list = NIL;
 static List *alter_type_exprs = NIL;
+static int	o_alter_generate_column_id = 0;
 Oid			o_saved_relrewrite = InvalidOid;
 Oid			o_saved_reltablespace = InvalidOid;
 List	   *o_reuse_indices = NIL;
@@ -873,6 +874,9 @@ orioledb_utility_command(PlannedStmt *pstmt,
 						case AT_SetStatistics:
 						case AT_SetUnLogged:
 						case AT_ValidateConstraint:
+#if PG_VERSION_NUM >= 170000
+						case AT_SetExpression:
+#endif
 							break;
 						case AT_DropOids:
 							ereport(WARNING,
@@ -890,9 +894,6 @@ orioledb_utility_command(PlannedStmt *pstmt,
 									(errcode(ERRCODE_SYNTAX_ERROR),
 									 errmsg("changing access method is not supported for OrioleDB tables")));
 							break;
-#if PG_VERSION_NUM >= 170000
-						case AT_SetExpression:
-#endif
 						case AT_SetCompression:
 						default:
 							ereport(ERROR,
@@ -2028,7 +2029,11 @@ rewrite_table(Relation rel, OTable *old_o_table, OTable *new_o_table)
 				expr = defaultexpr;
 			}
 
-			if (!expr && attr->attgenerated && old_slot->tts_isnull[i])
+			/*
+			 * Build new value for GENERATED column if calculating formula has been updated using
+			 * ALTER TABLE ... SET EXPRESSION ... or if value was not present in existing row
+			 */
+			if (!expr && attr->attgenerated && (old_slot->tts_isnull[i] || i == (o_alter_generate_column_id - 1)))
 			{
 				Node	   *defaultexpr = build_column_default(rel, i + 1);
 
@@ -2097,6 +2102,8 @@ rewrite_table(Relation rel, OTable *old_o_table, OTable *new_o_table)
 		ExecClearTuple(old_slot);
 		ExecClearTuple(new_slot);
 	}
+
+	o_alter_generate_column_id = 0;
 
 	ExecDropSingleTupleTableSlot(old_slot);
 	ExecDropSingleTupleTableSlot(new_slot);
@@ -3169,6 +3176,15 @@ orioledb_object_access_hook(ObjectAccessType access, Oid classId, Oid objectId,
 						if (ATColumnChangeRequiresRewrite(&old_field, field,
 														  subId))
 							in_rewrite = true;
+					}
+
+					/*
+					 * Alter table on generated column triggers table rewrite due to need of recalculating
+					 * column value for existing rows
+					 */
+					if (old_field.generated) {
+						in_rewrite = true;
+						o_alter_generate_column_id = subId;
 					}
 
 					if (!in_rewrite)
