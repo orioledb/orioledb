0d47df6db6085926325217b3fd913c88df533487
Pavel Borisov <pashkin.elfe@gmail.com>
2026-03-05 19:46:58 +0400

    Include OrioleDB tables size in counting database size
    Count database files in orioledb_data both in datadir and pg_tblspc/ dir
    
    NB: orioledb_undo is not per-database so it's not counted now

--- src/orioledb.c
+++ src/orioledb.c
@@ -151,6 +151,7 @@ int			rewind_max_time = 0;
 int			rewind_max_transactions = 0;
 int			logical_xid_buffers_guc = 64;
 bool		orioledb_strict_mode = false;
+database_size_hook_type prev_database_size_hook = NULL;
 
 /* Previous values of hooks to chain call them */
 static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
@@ -158,6 +159,7 @@ static void (*prev_shmem_request_hook) (void) = NULL;
 static base_init_startup_hook_type prev_base_init_startup_hook = NULL;
 static get_relation_info_hook_type prev_get_relation_info_hook = NULL;
 static skip_tree_height_hook_type prev_skip_tree_height_hook = NULL;
+
 CheckPoint_hook_type next_CheckPoint_hook = NULL;
 static bool o_newlocale_from_collation(void);
 
@@ -1173,6 +1175,8 @@ _PG_init(void)
 	waitSnapshotHook = orioledb_wait_snapshot;
 	GetReplayXlogPtrHook = recovery_get_effective_replay_ptr;
 
+	prev_database_size_hook = database_size_hook;
+	database_size_hook = orioledb_calculate_database_size;
 	RecoveryStopsBeforeHook = orioledb_recovery_stops_before_hook;
 
 	if (enable_rewind)
