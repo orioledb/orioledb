f63b752585ff6c28fcb9b05d5a46893725f65541
Pavel Borisov <pashkin.elfe@gmail.com>
2026-03-05 19:55:09 +0400

    Include OrioleDB tables size in counting database size
    Orioledb relations reside in a separate orioledb_data dir, for which
    calculate_database_size was unaware for.
    
    This patch implements hook function to count orioledb_data files both
     for datadir and pg_tblspc/ dir.
    
    As orioledb_undo is not per-database so it's not counted here. Use
    orioledb_undo_size() to get undo size for the database cluster.

--- test/sql/database.sql
+++ test/sql/database.sql
@@ -23,3 +23,88 @@ CREATE DATABASE orioledb TEMPLATE orioledb_template;
 DROP DATABASE orioledb_template;
 DROP DATABASE heapdb;
 DROP DATABASE heapdb_template;
+
+-- Check pg_database_size()
+CREATE EXTENSION orioledb;
+CREATE DATABASE oriole_database;
+\c oriole_database
+
+-- generate pseudo-random string function in deterministic way
+CREATE FUNCTION generate_string(seed integer, length integer) RETURNS text
+        AS $$
+                SELECT substr(string_agg(
+                                                substr(encode(sha256(seed::text::bytea || '_' || i::text::bytea), 'hex'), 1, 21),
+                                ''), 1, length)
+                FROM generate_series(1, (length + 20) / 21) i; $$
+LANGUAGE SQL;
+
+CHECKPOINT;
+select round(pg_database_size('oriole_database'), -6);
+
+CREATE EXTENSION orioledb;
+CREATE TABLE oriole_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb;
+INSERT INTO oriole_table(t) select generate_string(i, 270) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('oriole_database'), -6);
+
+SET allow_in_place_tablespaces = true;
+CREATE TABLESPACE dbsize_tblspace LOCATION '';
+CREATE TABLE oriole_table_tblspc (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb TABLESPACE dbsize_tblspace;
+INSERT INTO oriole_table_tblspc(t) select generate_string(i, 270) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('oriole_database'), -6);
+
+\d+ oriole_table
+\d+ oriole_table_tblspc
+
+\c postgres
+DROP DATABASE oriole_database;
+SELECT orioledb_rewind_sync();
+DROP TABLESPACE dbsize_tblspace;
+DROP EXTENSION orioledb CASCADE;
+
+CREATE EXTENSION orioledb;
+CREATE DATABASE mixed_database;
+\c mixed_database
+
+-- generate pseudo-random string function in deterministic way
+CREATE FUNCTION generate_string(seed integer, length integer) RETURNS text
+        AS $$
+                SELECT substr(string_agg(
+                                                substr(encode(sha256(seed::text::bytea || '_' || i::text::bytea), 'hex'), 1, 21),
+                                ''), 1, length)
+                FROM generate_series(1, (length + 20) / 21) i; $$
+LANGUAGE SQL;
+
+CREATE EXTENSION orioledb;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+CREATE TABLE heap_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING heap;
+INSERT INTO heap_table(t) select generate_string(i, 270) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+CREATE TABLE oriole_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb;
+INSERT INTO oriole_table(t) select generate_string(i, 270) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+SET allow_in_place_tablespaces = true;
+CREATE TABLESPACE dbsize_tblspace LOCATION '';
+
+CREATE TABLE oriole_table_tblspc (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb TABLESPACE dbsize_tblspace;
+INSERT INTO oriole_table_tblspc(t) select generate_string(i, 270) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+\d+ oriole_table
+\d+ heap_table
+\d+ oriole_table_tblspc
+
+\c postgres
+
+DROP DATABASE mixed_database;
+SELECT orioledb_rewind_sync();
+DROP TABLESPACE dbsize_tblspace;
+DROP EXTENSION orioledb CASCADE;
