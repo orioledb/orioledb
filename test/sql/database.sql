57252d2297d271107af82a20d636cda7f82ce2ce
Pavel Borisov <pashkin.elfe@gmail.com>
2026-03-05 19:55:09 +0400

    Add test for pg_database_size

--- test/sql/database.sql
+++ test/sql/database.sql
@@ -23,3 +23,60 @@ CREATE DATABASE orioledb TEMPLATE orioledb_template;
 DROP DATABASE orioledb_template;
 DROP DATABASE heapdb;
 DROP DATABASE heapdb_template;
+
+-- Check pg_database_size()
+CREATE DATABASE oriole_database;
+\c oriole_database
+
+-- generate pseudo-random string function in deterministic way
+CREATE FUNCTION generate_string(seed integer, length integer) RETURNS text
+        AS $$
+                SELECT substr(string_agg(
+                                                substr(encode(sha256(seed::text::bytea || '_' || i::text::bytea), 'hex'), 1, 21),
+                                ''), 1, length)
+                FROM generate_series(1, (length + 20) / 21) i; $$
+LANGUAGE SQL;
+
+CREATE EXTENSION orioledb;
+CHECKPOINT;
+select round(pg_database_size('oriole_database'), -6);
+
+CREATE TABLE oriole_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb;
+INSERT INTO oriole_table(t) select generate_string(i, 250) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('oriole_database'), -6);
+
+\d+ oriole_table
+\c postgres
+DROP DATABASE oriole_database;
+
+CREATE DATABASE mixed_database;
+\c mixed_database
+
+-- generate pseudo-random string function in deterministic way
+CREATE FUNCTION generate_string(seed integer, length integer) RETURNS text
+        AS $$
+                SELECT substr(string_agg(
+                                                substr(encode(sha256(seed::text::bytea || '_' || i::text::bytea), 'hex'), 1, 21),
+                                ''), 1, length)
+                FROM generate_series(1, (length + 20) / 21) i; $$
+LANGUAGE SQL;
+
+CREATE EXTENSION orioledb;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+CREATE TABLE heap_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING heap;
+INSERT INTO heap_table(t) select generate_string(i, 250) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+CREATE TABLE oriole_table (i SERIAL PRIMARY KEY, t text STORAGE PLAIN) USING orioledb;
+INSERT INTO oriole_table(t) select generate_string(i, 250) FROM  generate_series(1, 10000) as i;
+CHECKPOINT;
+select round(pg_database_size('mixed_database'), -6);
+
+\d+ oriole_table
+\d+ heap_table
+\c postgres
+DROP DATABASE mixed_database;
