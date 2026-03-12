cfc1b34f78db68c3d7cdd30918969512a4013e4f
Alexander Korotkov <akorotkov@postgresql.org>
2026-03-12 14:08:08 +0200

    Fix performance problem related to o(Tables|Indices)FetchCallback
    Returning OTupleFetchNotMatch for non-matching keys causes interator to go
    to the end of tree.  Return OTupleFetchMatch instead to immediately return
    non-matching tuple to the iterator and stop.

--- src/catalog/o_indices.c
+++ src/catalog/o_indices.c
@@ -189,7 +189,12 @@ oIndicesFetchCallback(OTuple tuple, OXid tupOxid, OSnapshot *oSnapshot,
 		else
 			return OTupleFetchNotMatch;
 	}
-	return OTupleFetchNotMatch;
+
+	/*
+	 * Return current tuple with unmatched key to iterator immediately to
+	 * finish the scan.
+	 */
+	return OTupleFetchMatch;
 }
 
 ToastAPI	oIndicesToastAPI = {
