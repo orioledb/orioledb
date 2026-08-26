dc0cd304f54562f0e0d2ab68143df034e949db3c
Ilya Kobets <i.kobets@orioledb.com>
2026-08-26 22:34:13 +0200

    Delete dead dropped_attrs list (Step 8 quick win)
    dropped_attrs became write-only dead after Step 7 Path B (commit 0506d07b):
    the OAT bridged toast branch that consumed it was deleted. Remove the decl,
    the append at the DROP COLUMN site, and the 4 free/NIL reset sites.

--- src/catalog/ddl.c
+++ src/catalog/ddl.c
@@ -139,7 +139,6 @@ static List *drop_index_list = NIL;
 static List *partition_drop_index_list = NIL;
 static List *alter_type_exprs = NIL;
 static List *o_alter_generated_column_id = NIL;
-static List *dropped_attrs = NIL;
 static bool o_composite_alter_index_safe = false;
 List	   *o_reuse_indices = NIL;
 static bool in_rewrite = false;
@@ -1090,7 +1089,6 @@ orioledb_utility_command(PlannedStmt *pstmt,
 		 * isn't freed by us and pointer may be invalid there
 		 */
 		alter_type_exprs = NIL;
-		dropped_attrs = NIL;
 
 		/*
 		 * ALTER TYPE ADD ATTRIBUTE on a composite type does not alter the
@@ -1783,11 +1781,6 @@ orioledb_utility_command(PlannedStmt *pstmt,
 			list_free(partition_drop_index_list);
 			partition_drop_index_list = NIL;
 		}
-		if (dropped_attrs)
-		{
-			list_free(dropped_attrs);
-			dropped_attrs = NIL;
-		}
 	}
 	else if (IsA(pstmt->utilityStmt, AlterTableStmt))
 	{
@@ -1803,9 +1796,6 @@ orioledb_utility_command(PlannedStmt *pstmt,
 
 		list_free_deep(o_alter_generated_column_id);
 		o_alter_generated_column_id = NIL;
-
-		list_free(dropped_attrs);
-		dropped_attrs = NIL;
 
 		o_composite_alter_index_safe = false;
 
@@ -4013,8 +4003,6 @@ orioledb_object_access_hook(ObjectAccessType access, Oid classId, Oid objectId,
 						o_tables_update(o_table, oxid, oSnapshot.csn);
 						o_tables_after_update(o_table, oxid, oSnapshot.csn);
 						o_tables_rel_meta_unlock(rel, InvalidOid);
-						/* cppcheck-suppress unknownEvaluationOrder */
-						dropped_attrs = lappend(dropped_attrs, list_make2(makeInteger(objectId), makeInteger(subId)));
 					}
 					o_table_free(o_table);
 				}
@@ -5781,11 +5769,6 @@ o_ddl_cleanup(void)
 	{
 		list_free_deep(reindex_list);
 		reindex_list = NIL;
-	}
-	if (dropped_attrs)
-	{
-		list_free(dropped_attrs);
-		dropped_attrs = NIL;
 	}
 	if (alter_type_exprs)
 	{
