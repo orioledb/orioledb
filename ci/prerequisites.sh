e3e4c2b26243027f1b9e3fb9c3d7909d1f59f4bf
Ilya Kobets <i.kobets@orioledb.com>
2026-09-11 16:07:45 +0200

    Moved apt update before apt install on ci

--- ci/prerequisites.sh
+++ ci/prerequisites.sh
@@ -24,9 +24,9 @@ while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
 	sleep 3
 done
 
-sudo apt-get -y install -qq wget ca-certificates
-
 sudo apt-get update -qq
+
+sudo apt-get -y install -qq wget ca-certificates
 
 apt_packages="build-essential flex bison pkg-config libreadline-dev make gdb libipc-run-perl libicu-dev python3-full python3-pip python3-setuptools python3-testresources libzstd1 libzstd-dev libcurl4-openssl-dev libssl-dev lcov"
 
